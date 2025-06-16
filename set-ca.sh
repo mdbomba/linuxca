c6a5fa3361aeb40ad7238eb750b955e6ac7a5059
Mike Bomba <32136488+mdbomba@users.noreply.github.com>
2025-06-16 00:50:59 -0700

    Updated 16 Jun 2025
    Test ran using sudo su;  cd /root.   Copy set-ca.sh and both openssl_.....cnf files to /root.

--- set-ca.sh
+++ set-ca.sh
@@ -1,86 +1,122 @@
 #!/bin/bash
 
-cd ~
+# Script to create a linux certificate authority
+# Version 20250616-02
+#
+# requires openssl_root.cnf and openssl_intermediate.cnf
+#
+# Function to read input with default value ( a = read_input {Prompt} {default} )
+function read_input {
+read -p "$1 [$2] = " i
+if [ "$i" != '' ]; then echo $i; else echo $2; fi
+}
+# Function to verify we are running as root
+function is-root {
+if [ $EUID -ne 0 ]; then 
+  echo "YOU NEED TO RUN THIS SCRIPT AS ROOT  (sudo)"
+  echo "Press any key to exit script"
+  read
+  exit
+else
+  echo "SCRIPT RUNNING AS ROOT (ALL GOOD)"
+fi
+}
 
-mkdir -p ~/myCA/rootCA/{certs,crl,newcerts,private,csr}
-mkdir -p ~/myCA/intermediateCA/{certs,crl,newcerts,private,csr}
-echo 1000 > ~/myCA/rootCA/serial
-echo 1000 > ~/myCA/intermediateCA/serial
-echo 0100 > ~/myCA/rootCA/crlnumber 
-echo 0100 > ~/myCA/intermediateCA/crlnumber
-touch ~/myCA/rootCA/index.txt
-touch ~/myCA/intermediateCA/index.txt
+# Terminate if not running as root (sudo)
+is-root
 
-# ROOT CA KEY AND CERTIFICATE GENERATION
+#########################
+# CONFIGURABLE PARAMETERS
+#########################
+
+## Config Files
+rootCACnf="/root/myCA/openssl_root.cnf"
+intermediateCACnf="/root/myCA/openssl_intermediate.cnf"
+
+## CERT SUBJECT INPUT
+rootCASubject="/C=US/ST=Arizona/L=Tombstone/O=Bomba Information Technology Services LLC/OU=Consulting/CN=Root CA"
+intermediateCASubject="/C=US/ST=Arizona/L=Tombstone/O=Bomba Information Technology Services LLC/OU=Consulting/CN=Intermediate CA"
+
+## NAME OF PEM CERTIFICATE FILES
+rootCACert="/root/myCA/rootCA/certs/ca.cert.pem"
+intermediateCACert="/root/myCA/intermediateCA/certs/intermediate.cert.pem"
+
+## NAME OF PEM KEY FILES
+rootCAKey="/root/myCA/rootCA/private/ca.key.pem"
+intermediateCAKey="/root/myCA/intermediateCA/private/intermediate.key.pem"
+
+## NAME OF INTERMEDIATE CERTIFICATE CSR FILE
+intermediateCACsr="/root/myCA/intermediateCA/csr/intermediate.csr"
+
+## NAME OF INTERMEDIATE CA CHAIN FILE
+intermediateCAChain="/root/myCA/intermediateCA/certs/intermediate.chain.pem"
+
+#########################################################
+# CREATE DIRECTORY STRUCTURE FOR root and intermediate CA
+#########################################################
+mkdir -p /root/myCA/rootCA/{certs,crl,newcerts,private,csr}
+mkdir -p /root/myCA/intermediateCA/{certs,crl,newcerts,private,csr}
+echo 1000 > /root/myCA/rootCA/serial
+echo 1000 > /root/myCA/intermediateCA/serial
+echo 0100 > /root/myCA/rootCA/crlnumber 
+echo 0100 > /root/myCA/intermediateCA/crlnumber
+touch /root/myCA/rootCA/index.txt
+touch /root/myCA/intermediateCA/index.txt
+
+
+cd /root/myCA
+
+
+# GENERATE ROOT CA
 
 echo "GENERATE ROOT CA PRIVATE KEY"
-openssl genrsa -out ~/myCA/rootCA/private/ca.key.pem 4096
-chmod 400 ~/myCA/rootCA/private/ca.key.pem
+openssl genrsa -out "$rootCAKey" 4096
+chmod 400 "$rootCAKey"
 
 echo "SHOW CONTENTS OF PRIVATE KEY"
-openssl rsa -noout -text -in ~/myCA/rootCA/private/ca.key.pem
+openssl rsa -noout -text -in "$rootCAKey"
 
 echo "GENERATE ROOT CA PUBLIC CERTIFICATE"
-openssl req -config openssl_root.cnf -key ~/myCA/rootCA/private/ca.key.pem -new -x509 -days 7300 -sha256 -extensions v3_ca -out ~/myCA/rootCA/certs/ca.cert.pem -subj "/C=US/ST=California/L=San Francisco/O=Example Corp/OU=IT Department/CN=Root CA"
-chmod 444 ~/myCA/rootCA/certs/ca.cert.pem
+openssl req -config openssl_root.cnf -key "$rootCAKey" -new -x509 -days 7300 -sha256 -extensions v3_ca -out "$rootCACert" -subj "$rootCASubject"
+chmod 444 "$rootCACert"
 
 echo "SHOW CONTENT OF ROOT PUBLIC CERTIFICATE"
-openssl x509 -noout -text -in ~/myCA/rootCA/certs/ca.cert.pem
+openssl x509 -noout -text -in "$rootCACert"
 
-# INTERMEDIATE CA KEY AND CERTIFICATE GENERATION
+
+# GENERATE INTERMEDIATE CA 
 
 echo "GENERATE INTERMEDIATE CA PRIVATE KEY"
-openssl genrsa -out ~/myCA/intermediateCA/private/intermediate.key.pem 4096
-chmod 400 ~/myCA/intermediateCA/private/intermediate.key.pem
+openssl genrsa -out "$intermediateCAKey" 4096
+chmod 400 "$intermediateCAKey"
 
 echo "GENERATE CSR TO REQUEST INTERMEDIATE CA PUBLIC CERTIFICATE"
-openssl req -config openssl_intermediate.cnf -key ~/myCA/intermediateCA/private/intermediate.key.pem -new -sha256 -out ~/myCA/intermediateCA/certs/intermediate.csr.pem -subj "/C=US/ST=California/L=San Francisco/O=Example Corp/OU=IT Department/CN=Intermediate CA"
+openssl req -config openssl_intermediate.cnf -key "$intermediateCAKey" -new -sha256 -out "$intermediateCACsr" -subj "$intermediateCASubject"
 
-echo "REQUEST INTERMEDIATE CA PUBLIC CERTIFICATE USING CSR FILE"
-openssl ca -config openssl_root.cnf -extensions v3_intermediate_ca -days 3650 -notext -md sha256 -in ~/myCA/intermediateCA/certs/intermediate.csr.pem -out ~/myCA/intermediateCA/certs/intermediate.cert.pem
-chmod 444 ~/myCA/intermediateCA/certs/intermediate.cert.pem
+echo "REQUEST INTERMEDIATE CA PUBLIC CERTIFICATE AND SIGN WITH ROOT KEY"
+openssl ca -config openssl_root.cnf -extensions v3_intermediate_ca -days 3650 -notext -md sha256 -in "$intermediateCACsr" -out "$intermediateCACert"
+chmod 444 "$intermediateCACert"
 
 echo "VERIFY CERTIFICATE FILE IS NOW LOCATED IN THE CA INDEX FILE"
 cat ~/myCA/rootCA/index.txt
 # SHOULD RETURN A LINE INCLUDING
-# V 330503082700Z 1000 unknown /C=US/ST=California/O=Example Corp/OU=IT Department/CN=Intermediate CA
+# V 330503082700Z 1000 unknown /C=US/ST=Arizona/L=Cochise/O=Bomba Information Technology Services LLC/OU=Consulting/CN=Intermediate CA
 
 echo "SHOW CONTENT OF INTERMEDIATE CA PUBLIC CERTIFICATE"
-openssl x509 -noout -text -in ~/myCA/intermediateCA/certs/intermediate.cert.pem
+openssl x509 -noout -text -in "$intermediateCACert"
 
 echo "USE OPENSSL VERIFY PROCESS TO VERIFY INTERMEDIATE CERTIFICATE"
-openssl verify -CAfile ~/myCA/rootCA/certs/ca.cert.pem ~/myCA/intermediateCA/certs/intermediate.cert.pem
+openssl verify -CAfile "$rootCACert" "$intermediateCACert"
 
 # CREATE TRUST CHAIN CERTIFICATE
 
 echo "CHREATE TRUST CHAIN FILE"
-cat ~/myCA/intermediateCA/certs/intermediate.cert.pem ~/myCA/rootCA/certs/ca.cert.pem > ~/myCA/intermediateCA/certs/ca-chain.cert.pem
+cat "$intermediateCACert" "$rootCACert" > "$intermediateCAChain"
 
 echo "USING OPENSSL VERIFY TO CHECK CHAIN FILE"
-openssl verify -CAfile ~/myCA/intermediateCA/certs/ca-chain.cert.pem ~/myCA/intermediateCA/certs/intermediate.cert.pem
-
-# CREATE SAMPLE WEB SERVER CERT TO VERIFY OVERALL CA FUNCTIONALITY
-
-echo "CREATE COMMON ATTRIBUTES IN openssl.cnf FILE"
+openssl verify -CAfile "$intermediateCAChain" "$intermediateCACert"
 
 
-
-echo "GENERATE WEB SERVER PRIVATE KEY"
-openssl genpkey -algorithm RSA -out ~/myCA/intermediateCA/private/www.example.com.key.pem
-chmod 400 ~/myCA/intermediateCA/private/www.example.com.key.pem
-
-echo "CREATE WEB SERVER CERTIFICATE REQUEST (CSR)"
-openssl req -config ~/myCA/openssl_intermediate.cnf -key ~/myCA/intermediateCA/private/www.example.com.key.pem -new -sha256 -out ~/myCA/intermediateCA/csr/www.example.com.csr.pem
-
-echo "REQUEST WEB SERVER CERT USING CSR FILE"
-openssl req -config ~/myCA/openssl_intermediate.cnf -key ~/myCA/intermediateCA/private/www.example.com.key.pem -new -sha256 -out ~/myCA/intermediateCA/csr/www.example.com.csr.pem -batch
-
-echo "SIGN WEB SERVER PUBLIC CERT USING INTERMEDIATE CERT"
-openssl ca -config ~/myCA/openssl_intermediate.cnf -extensions server_cert -days 375 -notext -md sha256 -in ~/myCA/intermediateCA/csr/www.example.com.csr.pem -out ~/myCA/intermediateCA/certs/www.example.com.cert.pem
-
-
-echo "VERIFY NEW SERVER CERTIFICATE"
-openssl x509 -noout -text -in ~/myCA/intermediateCA/certs/www.example.com.cert.pem
 
 
 
