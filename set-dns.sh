aa8de7483d44846c7b7e20ff367ea69226eeabb0
Mike Bomba <32136488+mdbomba@users.noreply.github.com>
2025-06-16 11:59:08 -0700

    Added config section

--- set-dns.sh
+++ set-dns.sh
@@ -2,8 +2,11 @@
 # Script to install dnsmasq and have it use the /etc/hosts file as its database
 # V 20250616-001
 
+# EDIT BELOW OPTIONS TO MEET YOUR NEEDS
 ip='10.5.5.60'
+int='enp1s0'
 
+# DO NOT EDIT BELOW THIS LINE
 sudo apt -y install dnsmasq
 sudo echo "
 port=53
@@ -11,7 +14,7 @@ no-resolv
 no-poll
 server=8.8.8.8
 server=8.8.4.4
-interface=enp1s0
+interface=$int
 listen-address=127.0.0.1,$ip
 bind-interfaces
 expand-hosts
